58a48344992117c85dfb7f5ec88bff17762baeb7
cpow <cpow85@gmail.com>
2013-01-03 12:25:10 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commoncore::Application.config.secret_token = 'e1afa29a5650df65f88917c9a401031c16a7f6efde6a1fb47b8ec3fae0b24cdd868186b7a43a20ddf2474c1393cf55b441dfcaba251dac405281cc2907c12e23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Commoncore::Application.config.session_store :cookie_store, key: '_commoncore_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Commoncore::Application.config.session_store :active_record_store
