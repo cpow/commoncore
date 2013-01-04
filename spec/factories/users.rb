1f60c084e2497a51ca2ebe16d1cad5fea7ba7d2d
cpow <cpow85@gmail.com>
2013-01-04 12:52:00 -0500

    added faker to user factory. got rid of using fixtures in the config file

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  password_and_confirmation = Faker::Base.bothify("Tlsakdjfh3425")
   factory :user do
-    name 'Test User'
-    email 'example@example.com'
-    password 'please'
-    password_confirmation 'please'
-    # required if the Devise Confirmable module is used
-    # confirmed_at Time.now
+    name Faker::Name.first_name
+    sequence(:email){|n| "user#{n}@factory.com"}
+    password password_and_confirmation
+    password_confirmation password_and_confirmation
   end
 end
