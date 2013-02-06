82c794cf1ad0cb94c54d526c7d92ddfbe3985d9d
cpow <cpow85@gmail.com>
2013-02-06 01:03:02 -0500

    added services for easier to read controller code with the logic of saving a very custom lesson plan model

---
+++ app/services/lesson_plan_saver.rb
@@ -0,0 +1,23 @@
+class LessonPlanSaver
+  attr_reader :params, :user, :lesson_plan
+  def initialize(opts={})
+    @params = opts[:params]
+    @user = opts[:user]
+    @lesson_plan = @user.lesson_plans.build(@params[:lesson_plan])
+  end
+
+  def save
+    add_core_standards_from_params
+    @lesson_plan.save
+  end
+
+  private
+
+    def add_core_standards_from_params
+      unless @params[:core_standard_ids].blank?
+        @params[:core_standard_ids].each do |id|
+          @lesson_plan.core_standards << CoreStandard.find(id)
+        end
+      end
+    end
+end
