460384211920dffc551e73972ae8c83a3878dd95
cpow <cpow85@gmail.com>
2013-02-21 23:20:25 -0500

    removed all the config stuff from post model. not needed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,25 +3,4 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
   validates :title, :body, :user_id, presence: true
-
-  rails_admin do
-    edit do
-      field :title
-
-      field :body, :text do
-        bootstrap_wysihtml5 true
-      end
-    
-      configure :user do
-        visible false
-      end
-  
-      field :user_id, :hidden do
-        visible true
-        default_value do
-          bindings[:view].current_user.id
-        end
-      end
-    end
-  end
 end
