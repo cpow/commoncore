d08991b014db65270799f503f31926240f1a278e
cpow <cpow85@gmail.com>
2013-06-10 09:19:13 -0400

    got tests to work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-  before_filter :only_admin_user
 
   def index
     @user = current_user
--- spec/features/admin_looks_at_posts_spec.rb
+++ spec/features/admin_looks_at_posts_spec.rb
@@ -5,6 +5,7 @@ feature 'admin looks at posts' do
     user = create_logged_in_user
     other_user = create :user
     user.add_role :admin
+    create :post, user: user
     visit user_home_index_path(user)
 
     expect(page).to have_css '.admin_posts'
