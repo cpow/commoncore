c8668b5f2783f8542b644d85db1c37f75c0c469a
cpow <cpow85@gmail.com>
2014-11-03 21:56:24 -0500

    no more admin_posts on home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @lesson_plans = @user.lesson_plans
-    @posts =  @user.has_role?(:blogger) ? @user.posts : []
-    @admin_posts =  @user.has_role?(:admin) ? Post.all : []
+    @posts =  @user.has_role?(:blogger) ? Post.all : []
   end
 end
