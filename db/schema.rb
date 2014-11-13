2e019a0abba1c05dff03bc2e69fa00f3e923169c
cpow <cpow85@gmail.com>
2014-11-13 18:34:40 -0500

    it wanted me to change my schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,157 +9,147 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141028033439) do
+ActiveRecord::Schema.define(version: 20141016002300) do
 
-  create_table "comments", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
     t.text     "content"
     t.integer  "commentable_id"
     t.string   "commentable_type"
     t.integer  "user_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "core_standards", :force => true do |t|
+  create_table "core_standards", force: true do |t|
     t.text     "statement"
     t.string   "docref_id"
     t.string   "guid"
     t.string   "uri"
     t.string   "related_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "dot_notation"
   end
 
-  create_table "core_standards_lesson_plans", :id => false, :force => true do |t|
+  create_table "core_standards_lesson_plans", id: false, force: true do |t|
     t.integer "lesson_plan_id"
     t.integer "core_standard_id"
   end
 
-  create_table "grade_levels", :force => true do |t|
+  create_table "grade_levels", force: true do |t|
     t.string   "level"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "leads", :force => true do |t|
+  create_table "leads", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "origin"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "lead_type"
   end
 
-  create_table "lesson_plans", :force => true do |t|
+  create_table "lesson_plans", force: true do |t|
     t.string   "name"
     t.string   "description"
     t.string   "lesson_plan_file"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
-    t.text     "objectives",       :default => ""
-    t.text     "agenda",           :default => ""
-    t.text     "materials",        :default => ""
+    t.text     "objectives",       default: ""
+    t.text     "agenda",           default: ""
+    t.text     "materials",        default: ""
   end
 
-  create_table "lesson_plans_core_standards", :id => false, :force => true do |t|
+  create_table "lesson_plans_core_standards", id: false, force: true do |t|
     t.integer "lesson_plan_id"
     t.integer "core_standard_id"
   end
 
-  create_table "levels", :force => true do |t|
+  create_table "levels", force: true do |t|
     t.integer  "grade_level_id"
     t.integer  "core_standard_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.text     "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
   end
 
-  create_table "products", :force => true do |t|
+  create_table "products", force: true do |t|
     t.integer  "user_id"
     t.string   "productable_type"
     t.integer  "productable_id"
     t.decimal  "price"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "rails_admin_histories", :force => true do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-  end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
-
-  create_table "roles", :force => true do |t|
+  create_table "roles", force: true do |t|
     t.string   "name"
     t.integer  "resource_id"
     t.string   "resource_type"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
-  add_index "roles", ["name"], :name => "index_roles_on_name"
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
-  create_table "stripe_auths", :force => true do |t|
+  create_table "stripe_auths", force: true do |t|
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "stripe_connects", :force => true do |t|
+  create_table "stripe_connects", force: true do |t|
     t.integer  "user_id"
     t.boolean  "livemode"
     t.string   "scope"
     t.string   "stripe_publishable_key"
     t.string   "token"
     t.boolean  "expires"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "uid"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "name"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "users_roles", :id => false, :force => true do |t|
+  create_table "users_roles", id: false, force: true do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
-  add_index "users_roles", ["user_id", "role_id"], :name => "index_users_roles_on_user_id_and_role_id"
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
 end
