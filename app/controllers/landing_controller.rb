3e527e979aa905c0400ea7e29aa64722274646fb
cpow <cpow85@gmail.com>
2014-12-27 11:50:14 -0500

    removed all references to a beta and replaced with account creation

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,8 @@
 class LandingController < ApplicationController
   def index
+    if current_user
+      redirect_to user_lesson_plans_path(
+        user_id: current_user.id)
+    end
   end
 end
--- spec/features/user_sees_dashboard_when_signed_in_spec.rb
+++ spec/features/user_sees_dashboard_when_signed_in_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 describe "seeing initial data" do
   before(:each) do
-    pending "waiting for beta"
     create_logged_in_user
   end
 
   it "should see initial data after logging in", js: true do
-    pending "waiting for beta"
     visit root_path
-    page.should have_content("Home")
+    page.should have_content("Lesson Plans")
   end
 end
