934ce3e9d4389c04c4a3c813f5a87ed0a1e049a8
cpow <cpow85@gmail.com>
2015-01-11 20:28:55 -0500

    adding changes to design

--- spec/features/admin_looks_at_posts_spec.rb
+++ spec/features/admin_looks_at_posts_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 feature 'admin looks at posts' do
   scenario 'sees all posts' do
     user = create_logged_in_user
-    other_user = create :user
     user.add_role :admin
     create :post, user: user
     visit user_home_index_path(user)
